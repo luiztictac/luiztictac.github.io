0d47d3f95cb80150349bd25acf4ba98e8b1c7722
= <=>
2021-08-03 01:12:39 -0300

    deploy

---
+++ src/app/models/os.ts
@@ -0,0 +1,10 @@
+export interface OS {
+    id?: any;
+    dataAbertura?: any;
+    DataFechamento?: any;
+    prioridade: any;
+    observacoes: String;
+    status: any;
+    tecnico: any;
+    cliente: any;
+}
